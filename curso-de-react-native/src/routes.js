aae15cce14188b2ed66b6c7995997430492bddbe
JoacyMS <joacymsilva@gmail.com>
2020-04-16 13:06:57 -0300

    Organização dos diretórios, instalação do 'react-navigation' e criação das rotas

---
+++ curso-de-react-native/src/routes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const AppStack = createStackNavigator();
+
+import Main from './pages/main';
+
+export default function Routes () {
+    return (
+        <NavigationContainer>
+
+            <AppStack.Navigator>
+                <AppStack.Screen name="Main" component={Main} />
+            </AppStack.Navigator>
+
+        </NavigationContainer>
+    );
+}
